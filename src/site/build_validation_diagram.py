700d7cda3f82f65db654e66b8306d5b35479e3ab
acapet <arthurcapet@gmail.com>
2026-06-07 15:59:28 +0200

    pp

--- src/site/build_validation_diagram.py
+++ src/site/build_validation_diagram.py
@@ -22,10 +22,10 @@ with g.subgraph(name="cluster_shared") as c:
     c.attr(label="Code Blue shared space")
     with c.subgraph(name="cluster_icesfolder") as c2:
         c2.attr(label="Formatted ICES In-situ data")
-        c2.node("STRUCT", "ICES_<VAR>_<YEAR>.parquet")
+        c2.node("STRUCT", "ICES_<VAR>_<YEAR>.parquet", shape="cylinder")
     with c.subgraph(name="cluster_CBF") as c3:
         c3.attr(label="Code Blue Files", shape="folder")
-        c3.node("CSV", "VALID_<VAR>_<YEAR>_<MODEL>.csv")
+        c3.node("CSV", "VALID_<VAR>_<YEAR>_<MODEL>.csv", shape="cylinder")
 
 with g.subgraph(name="cluster_partner") as c:
     c.attr(label="Code Blue partner")
